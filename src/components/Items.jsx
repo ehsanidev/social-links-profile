c08eafa8a22e4905896c9ce4e1bfbc46ebfd6a7f
sharkny <sharknystudio@gmail.com>
2026-05-27 07:27:51 +0900

    Updated

---
+++ src/components/Items.jsx
@@ -0,0 +1,25 @@
+<li className="social-links__item">
+          <a href="#" className="social-links__link">
+            GitHub
+          </a>
+        </li>
+        <li className="social-links__item">
+          <a href="#" className="social-links__link">
+            Frontend Mentor
+          </a>
+        </li>
+        <li className="social-links__item">
+          <a href="#" className="social-links__link">
+            LinkedIn
+          </a>
+        </li>
+        <li className="social-links__item">
+          <a href="#" className="social-links__link">
+            Twitter
+          </a>
+        </li>
+        <li className="social-links__item">
+          <a href="#" className="social-links__link">
+            Instagram
+          </a>
+        </li>
